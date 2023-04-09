f3f63d33ce631ebf4f8c044d6aadfdec45de6cee
Ahmad shraim <103267848+962ahmad@users.noreply.github.com>
2023-04-10 02:00:05 +0300

    Add files via upload
    VP-ASSIGNMENT_2

---
+++ VP-ASSIGNMENT_2-main/ASSIGNMENT-2/ASSIGNMENT-2/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ASSIGNMENT_2
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPassWord = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.lblPassWord = new System.Windows.Forms.Label();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(355, 264);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(180, 55);
+            this.btnLogin.TabIndex = 9;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtPassWord
+            // 
+            this.txtPassWord.Location = new System.Drawing.Point(285, 191);
+            this.txtPassWord.Name = "txtPassWord";
+            this.txtPassWord.Size = new System.Drawing.Size(342, 26);
+            this.txtPassWord.TabIndex = 8;
+            this.txtPassWord.UseSystemPasswordChar = true;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(285, 124);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(342, 26);
+            this.txtUserName.TabIndex = 7;
+            // 
+            // lblPassWord
+            // 
+            this.lblPassWord.AutoSize = true;
+            this.lblPassWord.Location = new System.Drawing.Point(176, 194);
+            this.lblPassWord.Name = "lblPassWord";
+            this.lblPassWord.Size = new System.Drawing.Size(86, 20);
+            this.lblPassWord.TabIndex = 6;
+            this.lblPassWord.Text = "Password";
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(173, 127);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(98, 20);
+            this.lblUserName.TabIndex = 5;
+            this.lblUserName.Text = "User Name";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassWord);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblPassWord);
+            this.Controls.Add(this.lblUserName);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmLogin";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPassWord;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lblPassWord;
+        private System.Windows.Forms.Label lblUserName;
+    }
+}
+
---
+++ VP-ASSIGNMENT_2-main/ASSIGNMENT-2/ASSIGNMENT-2/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASSIGNMENT_2
+{
+    public partial class frmLogin : Form
+    {
+
+        private User user1;
+        private User user2;
+        private User user3;
+        private User user4;
+        private User user5;
+
+        private List<User> users;
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            user1 = new User();
+            user1.UserId = 1;
+            user1.FullName = "Khaled Mohammad";
+            user1.Username = "KM";
+            user1.Password = "1234";
+
+            user2 = new User();
+            user2.UserId = 2;
+            user2.FullName = "AHMAD SHRAIM";
+            user2.Username = "AS";
+            user2.Password = "1234";
+
+            user3 = new User();
+            user3.UserId = 3;
+            user3.FullName = "AMMAR JABER";
+            user3.Username = "AJ";
+            user3.Password = "1234";
+
+            user4 = new User();
+            user4.UserId = 4;
+            user4.FullName = "Ibrahem Awad";
+            user4.Username = "IA";
+            user4.Password = "1234";
+
+            user5 = new User();
+            user5.UserId = 5;
+            user5.FullName = "Suad Ahmad";
+            user5.Username = "SA";
+            user5.Password = "1234";
+
+            users = new List<User>();
+
+            users.Add(user1);
+            users.Add(user2);
+            users.Add(user3);
+            users.Add(user4);
+            users.Add(user5);
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            int index = -1;
+            for (int i = 0; i < users.Count; i++)
+            {
+                if (users[i].Username.ToLower() != txtUserName.Text.ToLower())
+                {
+                    index = -1;
+                    
+                }
+                else
+                {
+                    if (users[i].Password == txtPassWord.Text)
+                    {
+                        index = 0;
+                        MessageBox.Show("Welcome " + users[i].FullName);
+                        break;
+                    }
+                    else
+                    {
+                        index = -2;
+                        break;
+                    }
+                }
+            }
+            if (index == -1) 
+            {
+                MessageBox.Show("User Dosen't exist");
+            }
+            else if (index == -2)
+            {
+                MessageBox.Show("Wrong Password");
+            }
+
+            
+        }
+    }
+}
